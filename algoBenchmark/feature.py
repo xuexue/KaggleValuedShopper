5b7a6d032ad4b5a8e38035b9ba56da8efb1282c5
Lisa Zhang <lisa@polychart.com>
2014-06-12 18:06:18 -0400

    try to speed up feature gen

--- algoBenchmark/feature.py
+++ algoBenchmark/feature.py
@@ -1,8 +1,9 @@
 '''Generate features CSV for the appropriate data'''
 import argparse
-import dateutil.parser
-import csv
+import time
 from collections import defaultdict
+
+DATEFORMAT = "%Y-%m-%d"
 
 # GLOBALS
 keys_tmpl = ['has_bought_%s', 'has_bought_%s_a', 'has_bought_%s_q',
@@ -35,39 +36,61 @@ def write_data(outfile, id, ftrs, historydict, HAS_REAL_VALUE=True):
     towrite = [id] + features
     if HAS_REAL_VALUE:
         towrite += historydict[id]['repeater']
-    outfile.writerow(towrite)
+    outfile.write(','.join(str(x) for x in towrite) + '\n')
 
 def run(offers, history, transactions, out):
     HAS_REAL_VALUE = False
 
     print('read offers into memory')
     offersdict = {}
-    offersfile = csv.DictReader(open(offers))
-    for item in offersfile:
-        offersdict[item['offer']] = item
+    offersfile = open(offers)
+    offersfile.next() # header
+    for line in offersfile:
+        line = line.strip().split(",")
+        offersdict[line[0]] = {
+                'category': line[1],
+                'company': line[3],
+                'offervalue': line[4],
+                'brand': line[5],
+                }
 
     print('read training data info into memory')
     historydict = {}
-    historyfile = csv.DictReader(open(history))
-    HAS_REAL_VALUE = ('repeater' in historyfile.fieldnames)
-    for item in historyfile:
-        item['offerdate'] = dateutil.parser.parse(item['offerdate'])
-        historydict[item['id']] = item
+    historyfile = open(history)
+    header = historyfile.next()
+    HAS_REAL_VALUE = ('repeater' in header.strip().split(','))
+    for line in historyfile:
+        line = line.strip().split(',')
+        historydict[line[0]] = {
+                'offer': line[2],
+                'offerdate': time.mktime(time.strptime(line[4], DATEFORMAT))
+                }
 
     print('set up the transaction file %s and output file %s' % (transactions, out))
     headers = ['id'] + keys
     if HAS_REAL_VALUE:
         headers += ['repeater']
-    outfile = csv.writer(open(out, 'w'))
-    outfile.writerow(headers)
+    outfile = open(out, 'w')
+    outfile.write(','.join(headers) + '\n')
 
     print('go through the transaction file and populate features')
-    transactfile = csv.DictReader(open(transactions))
+    transactfile = open(transactions)
+    transactfile.next() # header
     lastID = None
     i = 0
     ftrs = defaultdict(lambda: 0) # all default values happen to be zero
-    for event in transactfile:
-        id = event['id']
+    for line in transactfile:
+        line = line.strip().split(',')
+        id = line[0]
+        event = {
+                'id': id,
+                'date': line[6],
+                'category': line[3],
+                'company': line[4],
+                'brand': line[5],
+                'purchasequantity': float(line[-2]),
+                'purchaseamount': float(line[-1])
+        }
 
         if id != lastID and lastID is not None:
             fill_derived_features(ftrs)
@@ -76,19 +99,21 @@ def run(offers, history, transactions, out):
             i += 1
             if i % 1000 == 0:
                 print i
+            if i == 5000:
+                break
         lastID = id
 
         offerid = historydict[id]['offer']
         curroffer = offersdict[offerid]
 
-        datediff = historydict[id]['offerdate'] - dateutil.parser.parse(event['date'])
-        datediff = datediff.days
+        dt = time.mktime(time.strptime(event['date'], DATEFORMAT))
+        datediff = (historydict[id]['offerdate'] - dt) / (60*60*24)
 
         for col in ['company', 'category', 'brand']:
             if curroffer[col] == event[col]:
                 ftrs['has_bought_%s' % col] += 1
-                ftrs['has_bought_%s_a' % col] += float(event['purchaseamount'])
-                ftrs['has_bought_%s_q' % col] += float(event['purchasequantity'])
+                ftrs['has_bought_%s_a' % col] += event['purchaseamount']
+                ftrs['has_bought_%s_q' % col] += event['purchasequantity']
                 if datediff < 30:
                     ftrs['has_bought_%s_30' % col] += 1
                 if datediff < 60:
@@ -101,8 +126,8 @@ def run(offers, history, transactions, out):
                     ftrs['has_bought_%s_150' % col] += 1
                 if datediff < 180:
                     ftrs['has_bought_%s_180' % col] += 1
-        ftrs['total_spend'] += float(event['purchaseamount'])
-        ftrs['total_quantity'] += float(event['purchasequantity'])
+        ftrs['total_spend'] += event['purchaseamount']
+        ftrs['total_quantity'] += event['purchasequantity']
         ftrs['total_purchases'] += 1
         ftrs['offer_value'] = curroffer['offervalue'] # kind of repeat
 
@@ -118,4 +143,5 @@ if __name__ == "__main__":
     parser.add_argument('--out', dest='out', default=None)
     args = parser.parse_args()
 
+    #import cProfile
     run(args.offers, args.history, args.transactions, args.out)
