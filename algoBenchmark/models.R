5ace739df30bd775273f037550910e371cc317ed
Lisa Zhang <lisa@polychart.com>
2014-06-12 22:11:49 -0400

    add more experimential R code

--- algoBenchmark/models.R
+++ algoBenchmark/models.R
@@ -5,13 +5,10 @@ args <- commandArgs(trailingOnly = TRUE)
 trainingfile <- args[0]
 testfile <- args[1]
 
-trainingfile <- '../interm/benchmarkFeaturesTrain_'
-testfile <- '../interm/benchmarkFeaturesTest_'
+trainingfile <- '../interm/benchmarkFeaturesTrain'
+testfile <- '../interm/benchmarkFeaturesTest'
 
 deleteOldRows <- function(df) {
-  df$has_never_bought_companY_brand <- NULL
-  df$has_never_bought_category_brand <- NULL
-  df$has_never_bought_category_company <- NULL
   df$repeater <- (as.character(df$repeater) == 't')
   return(df)
 }
@@ -27,19 +24,46 @@ library(quantreg)
 model.qt <- rq(repeater ~ ., tau= .5, data=trainingdf)
 pred.qt <- predict(model.qt, testdf)
 pred.qt <- pmax(0, pmin(1, pred.qt))
-performance(prediction(pred.qt, testdf$repeater), 'auc')  # == 0.5946262694 (w/out 3)
+performance(prediction(pred.qt, testdf$repeater), 'auc') # == 0.5963949198
+# == 0.5946262694 (w/out 3)
 
 ##### dumb logistic regression model
 model.glm <- glm(repeater ~ ., data=trainingdf, family=binomial(link=logit))
 summary(model.glm)
 pred.glm <- predict(model.glm, testdf, type="response")
-performance(prediction(pred.glm, testdf$repeater), 'auc') # == 0.5311540869
+performance(prediction(pred.glm, testdf$repeater), 'auc') # == 0.5039108188 (wtf?)
+# == 0.5311540869 (w/out 3)
 
 ##### random forest
 library(randomForest)
-model.rf <- randomForest(as.factor(repeater) ~ ., data=trainingdf, ntree=500, maxnodes=30, nodesize=2, importance=T)
+rfsubset <- trainingdf
+# 30's
+rfsubset$has_bought_category_company <- NULL
+rfsubset$has_bought_category_brand <- NULL
+rfsubset$has_never_bought_brand <- NULL
+# 40's
+rfsubset$has_bought_company_brand <- NULL
+rfsubset$has_never_bought_company <- NULL
+rfsubset$has_bought_company_180 <- NULL
+rfsubset$has_bought_company_150 <- NULL
+rfsubset$has_bought_company_150 <- NULL
+# 50's
+#rfsubset$has_bought_brand_a <- NULL
+#rfsubset$has_bought_brand_q <- NULL
+#rfsubset$total_purchases <- NULL
+model.rf <- randomForest(as.factor(repeater) ~ ., data=rfsubset, ntree=500, maxnodes=30, nodesize=2, importance=T)
 pred.rf <- predict(model.rf, testdf, type="prob")[,"TRUE"]
 performance(prediction(pred.rf, testdf$repeater), 'auc') 
+# RESULT WITH "BEST SUBSET" OF THE FEATURES 
+# 500/30/2 == 0.5897112426 (remove GINI < 40 from *) **
+# 500/30/2 == 0.5887868002 (remove GINI < 50 from **)
+
+# RESULT WITH "ALL" OF THE FEATURES
+# 500/25/2 == 0.584597299
+# 500/35/2 == 0.5853384781
+# 500/30/2 == 0.5880069463*
+# 50/30/2 == 0.5762595821
+# 10/30/2 == 0.5707286909
 
 # RESULT WITHOUT THREE OF THE FEATURES
 # 500/30/2 == 0.5889198419
@@ -51,3 +75,40 @@ performance(prediction(pred.rf, testdf$repeater), 'auc')
 # 5/20/10 == 0.5632263927
 # 5/30/10 == 0.5595678066
 
+##### COMBINATIONS
+
+## Linear combination
+pred.comb.linear <- 0.5 * (pred.rf + pred.qt)
+performance(prediction(pred.comb.linear, testdf$repeater), 'auc')  # == 0.6102466827
+
+## GAM based on the result
+library(mgcv)
+df.comb.train <- data.frame(
+  qt=predict(model.qt, trainingdf),
+  rf=predict(model.rf, trainingdf, type="prob")[,"TRUE"],
+  y=trainingdf$repeater)
+df.comb.test <- data.frame(qt=pred.qt, rf=pred.rf)
+model.comb.gam <- gam(y ~ s(qt) + s(rf), data=df.comb.train)
+pred.comb.gam <- predict(model.comb.gam, df.comb.test)
+pred.comb.gam <- pmin(1, pmax(0, pred.comb.gam))
+performance(prediction(pred.comb.gam, testdf$repeater), 'auc') # == 0.611884483
+
+model.comb.lm <- lm(y ~ qt + rf, data=df.comb.train)
+pred.comb.lm <- predict(model.comb.lm , df.comb.test, type="response")
+pred.comb.lm <- pmin(1, pmax(0, pred.comb.lm))
+performance(prediction(pred.comb.lm, testdf$repeater), 'auc') # == 0.6102897732
+
+model.comb.glm <- glm(y ~ qt + rf, data=df.comb.train, family=binomial(link="logit"))
+pred.comb.glm <- predict(model.comb.glm, df.comb.test, type="response")
+pred.comb.glm <- pmin(1, pmax(0, pred.comb.glm))
+performance(prediction(pred.comb.glm, testdf$repeater), 'auc') # == 0.6117167186
+
+model.comb.gam <- gam(y ~ s(qt) + s(rf), data=df.comb.train, family=binomial(link="logit"))
+pred.comb.gam <- predict(model.comb.gam, df.comb.test, type="response")
+pred.comb.gam <- pmin(1, pmax(0, pred.comb.gam))
+performance(prediction(pred.comb.gam, testdf$repeater), 'auc') # == 0.611884483
+
+model.comb.gam <- gam(y ~ s(qt) + s(rf), data=df.comb.train)
+pred.comb.gam <- predict(model.comb.gam, df.comb.test)
+pred.comb.gam <- pmin(1, pmax(0, pred.comb.gam))
+performance(prediction(pred.comb.gam, testdf$repeater), 'auc') # == 0.6118256194
